8c800b640d5408548a327b66dcc430867db0cba2
avasilevskaya <anst.vasilevskaya@gmail.com>
2022-05-02 17:03:05 +0300

    refactor: stack: remove commented lines

--- src/stack.js
+++ src/stack.js
@@ -13,9 +13,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack extends Array{
-  //constructor() {
-  //  this.container = [];
-  //}
   static get [Symbol.species]() {
     return Array;
   }
@@ -24,19 +21,6 @@ class Stack extends Array{
     return top;
   }
 }
-
-/*
-const stack = new Stack();
-console.log(stack);
-stack.push(5);
-stack.push(6);
-stack.push(7);
-console.log(stack);
-console.log(stack.pop());
-console.log(stack);
-console.log(stack.peek());
-console.log(stack);
-*/
 
 module.exports = {
   Stack
